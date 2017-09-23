931c432e5138a96a70a4e83206ad7dfc18c3346e
gawa <precpaw@op.pl>
2017-09-23 16:25:14 +0200

    Clearing files.

--- src/main/resources/distro/runImdb.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-echo 'Start program'
-echo '.............'
-
-java -jar DownloadStationHelper-core-1.0.jar $(cat settings/credentials) imdbMode
-
-echo '.............'
-echo 'Program ended'
