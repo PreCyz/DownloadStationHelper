3483966f4b03fdf955e7d94c2cb3413c6bcdce15
gawa <precpaw@op.pl>
2017-10-07 17:59:11 +0200

    Adding FileUtils.

---
+++ src/main/java/pg/util/FileUtils.java
@@ -0,0 +1,13 @@
+package pg.util;
+
+/**Created by Gawa 2017-10-07*/
+public final class FileUtils {
+
+    private FileUtils() {}
+
+    public static void createApplicationProperties() {
+
+    }
+
+    public static void createShowProperty() {}
+}
