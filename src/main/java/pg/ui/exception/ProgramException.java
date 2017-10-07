d9da0d3fd0163f71e540766d9b2796fe2db964ca
gawa <precpaw@op.pl>
2017-10-07 21:25:43 +0200

    Adding ProgramException.

---
+++ src/main/java/pg/ui/exception/ProgramException.java
@@ -0,0 +1,27 @@
+package pg.ui.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**Created by Gawa 2017-10-07*/
+public class ProgramException extends RuntimeException {
+    private final UIError uiError;
+    private Map<UIError, String> arguments = new HashMap<>();
+
+    public ProgramException(UIError uiError, String arguments, Exception exception) {
+        super(exception);
+        this.uiError = uiError;
+        this.arguments.put(uiError, arguments);
+    }
+
+    public UIError getUiError() {
+        return uiError;
+    }
+
+    @Override
+    public String getMessage() {
+        StringBuilder stringBuilder = new StringBuilder("Error occurred: ");
+        arguments.forEach((key, value) -> stringBuilder.append(key).append(" - ").append(value).append(", "));
+        return stringBuilder.substring(0, stringBuilder.lastIndexOf(", "));
+    }
+}
