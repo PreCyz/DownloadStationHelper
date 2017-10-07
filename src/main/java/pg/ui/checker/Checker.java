08caac1dd42e35fbf2621fe6571d7bf6f6e9bb4d
gawa <precpaw@op.pl>
2017-10-07 18:00:28 +0200

    Adding Checkers.

---
+++ src/main/java/pg/ui/checker/Checker.java
@@ -0,0 +1,6 @@
+package pg.ui.checker;
+
+/**Created by Gawa 2017-10-07*/
+public interface Checker {
+    boolean ready();
+}
---
+++ src/main/java/pg/ui/checker/ShowChecker.java
@@ -0,0 +1,10 @@
+package pg.ui.checker;
+
+/**Created by Gawa 2017-10-07*/
+public class ShowChecker implements Checker {
+
+    @Override
+    public boolean ready() {
+        return false;
+    }
+}
