c99fc24ff8edd4a16a3f757d80ada7ef46357218
gawa <precpaw@op.pl>
2017-10-07 21:25:25 +0200

    Adding UIError.

---
+++ src/main/java/pg/ui/exception/UIError.java
@@ -0,0 +1,16 @@
+package pg.ui.exception;
+
+/**Created by Gawa 2017-10-07*/
+public enum UIError {
+    LOAD_BUNDLE,
+    LOAD_PROPERTIES,
+    SAVE_PROPERTIES,
+    NULL_ARGUMENT,
+    NO_ARGUMENTS,
+    PREPARATION,
+    NULL_ERROR_CODE,
+    BUTTON_IMG,
+    DESTINATION_NOT_CREATED,
+    LAUNCH_PROGRAM,
+    ARGUMENT_WRONG_VALUE
+}
