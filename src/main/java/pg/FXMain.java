2a5dd7582a0edd124553a16989c92b2b74427aae
gawa <precpaw@op.pl>
2017-10-07 17:58:58 +0200

    Adding FXMain.

---
+++ src/main/java/pg/FXMain.java
@@ -0,0 +1,18 @@
+package pg;/**
+ * Created by Gawa 2017-10-04
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class FXMain extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
