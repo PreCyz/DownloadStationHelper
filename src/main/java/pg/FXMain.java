57c8314a32489353914f7f172df55cd3e643e0e8
gawa <precpaw@op.pl>
2017-10-07 21:28:56 +0200

    Adding proper window order launch.

--- src/main/java/pg/FXMain.java
+++ src/main/java/pg/FXMain.java
@@ -1,10 +1,14 @@
-package pg;/**
- * Created by Gawa 2017-10-04
- */
+package pg;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import pg.ui.checker.ApplicationChecker;
+import pg.ui.checker.Checker;
+import pg.ui.checker.ShowChecker;
+import pg.ui.handler.WindowHandler;
+import pg.ui.handler.WindowHandlerImpl;
 
+/** Created by Gawa 2017-10-04*/
 public class FXMain extends Application {
 
     public static void main(String[] args) {
@@ -13,6 +17,15 @@ public class FXMain extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-
+        WindowHandler windowHandler = new WindowHandlerImpl(primaryStage);
+        Checker applicationProperties = new ApplicationChecker();
+        if (!applicationProperties.ready()) {
+            windowHandler.launchConfigWindow();
+        }
+        Checker showProperties = new ShowChecker();
+        if (!showProperties.ready()) {
+            windowHandler.launchShowWindow();
+        }
+        windowHandler.launchMainWindow();
     }
 }
