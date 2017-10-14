c6c04b3e9293774114920340fd110880176c07b3
gawa <precpaw@op.pl>
2017-10-14 21:10:17 +0200

    Adding logger to controllers.

--- src/main/java/pg/ui/controller/AbstractController.java
+++ src/main/java/pg/ui/controller/AbstractController.java
@@ -1,6 +1,8 @@
 package pg.ui.controller;
 
 import javafx.fxml.Initializable;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import pg.ui.handler.WindowHandler;
 
 import java.net.URL;
@@ -9,12 +11,14 @@ import java.util.ResourceBundle;
 /**Created by Gawa 2017-10-04*/
 public abstract class AbstractController implements Initializable {
 
+    protected final Logger logger;
     protected final WindowHandler windowHandler;
     protected URL location;
     protected ResourceBundle bundle;
 
     protected AbstractController(WindowHandler windowHandler) {
         this.windowHandler = windowHandler;
+        this.logger = LogManager.getLogger(getClass());
     }
 
     @Override
