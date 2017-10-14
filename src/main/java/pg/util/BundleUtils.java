fd6936b39782710b52eced4b447040dba9a12802
gawa <precpaw@op.pl>
2017-10-14 19:44:42 +0200

    Introducing BundleUtils.

---
+++ src/main/java/pg/util/BundleUtils.java
@@ -0,0 +1,29 @@
+package pg.util;
+
+import pg.ui.exception.ProgramException;
+import pg.ui.exception.UIError;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**Created by Gawa 2017-10-14*/
+public final class BundleUtils {
+
+    private BundleUtils() {}
+
+    public static ResourceBundle readBundles() throws ProgramException {
+        ResourceBundle bundle = ResourceBundle.getBundle(AppConstants.BUNDLE_PATH);
+        if (bundle == null) {
+            throw new ProgramException(UIError.LOAD_BUNDLE);
+        }
+        return bundle;
+    }
+
+    public static ResourceBundle readBundles(Locale locale) throws ProgramException {
+        ResourceBundle bundle = ResourceBundle.getBundle(AppConstants.BUNDLE_PATH, locale);
+        if (bundle == null) {
+            throw new ProgramException(UIError.LOAD_BUNDLE);
+        }
+        return bundle;
+    }
+}
