c7979f4abdee5238187105383095e4c79f5b3e29
Gawa <precpaw@op.pl>
2017-10-28 15:37:12 +0200

    Implementing ShowChecker.

--- src/main/java/pg/ui/checker/ShowChecker.java
+++ src/main/java/pg/ui/checker/ShowChecker.java
@@ -1,10 +1,18 @@
 package pg.ui.checker;
 
+import pg.props.ShowsPropertiesHelper;
+
 /**Created by Gawa 2017-10-07*/
 public class ShowChecker implements Checker {
 
+    private ShowsPropertiesHelper helper;
+
+    public ShowChecker() {
+        this.helper = ShowsPropertiesHelper.getInstance();
+    }
+
     @Override
     public boolean ready() {
-        return false;
+        return !helper.keySet().isEmpty();
     }
 }
