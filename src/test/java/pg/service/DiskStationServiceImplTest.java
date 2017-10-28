f040e36abce693f4e3f390d70d02ecbae6d82831
Gawa <precpaw@op.pl>
2017-10-28 10:00:07 +0200

    Unit test for calculating size with streams.

--- src/test/java/pg/service/DiskStationServiceImplTest.java
+++ src/test/java/pg/service/DiskStationServiceImplTest.java
@@ -1,14 +1,23 @@
 package pg.service;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+import pg.web.model.torrent.TorrentDetail;
+import pg.web.response.TorrentResponse;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**Created by Gawa 2017-09-15*/
+@RunWith(MockitoJUnitRunner.class)
 public class DiskStationServiceImplTest {
 
     private DiskStationServiceImpl diskStationService;
@@ -23,4 +32,14 @@ public class DiskStationServiceImplTest {
         assertThat(actual, is( equalTo("http://some.address.com:5000")));
     }
 
+    @Test
+    public void givenListInsideListWhenCalculatingSizeOfTheCollectionWithTheStreamsThenReturnProperSize() {
+        TorrentResponse tr = mock(TorrentResponse.class);
+        when(tr.getTorrents()).thenReturn(
+                Arrays.asList(new TorrentDetail(), new TorrentDetail(), new TorrentDetail())
+        );
+        List<TorrentResponse> responses = Collections.singletonList(tr);
+
+        assertThat(3, is( equalTo(responses.stream().mapToInt(r->r.getTorrents().size()).sum())));
+    }
 }
