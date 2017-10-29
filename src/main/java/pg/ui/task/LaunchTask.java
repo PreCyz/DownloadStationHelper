2daaf26876402c2b265d9d60750007f6cac175da
Gawa <precpaw@op.pl>
2017-10-29 14:05:34 +0100

    Adding LaunchTask.

---
+++ src/main/java/pg/ui/task/LaunchTask.java
@@ -0,0 +1,99 @@
+package pg.ui.task;
+
+import javafx.concurrent.Task;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import pg.factory.MatchServiceFactory;
+import pg.factory.TorrentServiceFactory;
+import pg.props.ApplicationPropertiesHelper;
+import pg.service.DiskStationService;
+import pg.service.DiskStationServiceImpl;
+import pg.service.FileService;
+import pg.service.FileServiceImpl;
+import pg.service.match.MatchService;
+import pg.service.torrent.TorrentService;
+import pg.util.StringUtils;
+import pg.web.model.DSMethod;
+import pg.web.model.ProgramMode;
+import pg.web.model.torrent.TorrentDetail;
+
+import java.util.List;
+
+/** Created by Gawa 2017-10-29 */
+public class LaunchTask<Void> extends Task<Void> {
+
+    private static final Logger logger = LogManager.getLogger(LaunchTask.class);
+
+    private String imdbId;
+    private final ProgramMode programMode;
+    private final ApplicationPropertiesHelper application;
+
+    public LaunchTask() {
+        this.programMode = ProgramMode.ALL_CONCURRENT;
+        application = ApplicationPropertiesHelper.getInstance();
+    }
+
+    public LaunchTask(String imdbId) {
+        this.programMode = ProgramMode.IMDB;
+        this.imdbId = imdbId;
+        application = ApplicationPropertiesHelper.getInstance();
+    }
+
+    @Override
+    protected Void call() throws Exception {
+        TorrentService torrentService;
+        if (StringUtils.nullOrTrimEmpty(imdbId)) {
+            torrentService = TorrentServiceFactory.getTorrentService(programMode);
+        } else {
+            torrentService = TorrentServiceFactory.getTorrentService(imdbId);
+        }
+        MatchService matchService = MatchServiceFactory.getMatchService(programMode);
+        List<TorrentDetail> torrents = torrentService.findTorrents();
+        FileService fileService = new FileServiceImpl();
+        fileService.buildImdbMap(torrents);
+        fileService.writeImdbMapToFile();
+        matchService.match(torrents);
+        if (matchService.hasFoundMatchingTorrents()) {
+            final String notGiven = "NOT_GIVEN";
+            final String filePath = application.getFilePath(notGiven);
+            if (!notGiven.equals(filePath.trim())) {
+                fileService.writeTorrentsToFile(matchService.getMatchingTorrents());
+            }
+            String creationMethod = application.getCreationMethod(DSMethod.COPY_FILE.name());
+            DiskStationService dsService = new DiskStationServiceImpl(matchService.getMatchingTorrents());
+            switch (DSMethod.valueOf(creationMethod)) {
+                case COPY_FILE:
+                    dsService.writeTorrentsOnDS();
+                    break;
+                case REST:
+                    dsService.prepareAvailableOperations();
+                    dsService.loginToDiskStation();
+                    dsService.createDownloadStationTasks();
+                    dsService.listOfTasks();
+                    dsService.logoutFromDiskStation();
+                    break;
+            }
+        } else {
+            logger.info("No matching torrents found.");
+        }
+        return null;
+    }
+
+    @Override
+    protected void running() {
+        super.running();
+        logger.info("Task is running.");
+    }
+
+    @Override
+    protected void cancelled() {
+        super.cancelled();
+        logger.info("Task is cancelled.");
+    }
+
+    @Override
+    protected void succeeded() {
+        super.succeeded();
+        logger.info("Task is succeeded.");
+    }
+}
