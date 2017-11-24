150c3ecdb3c3acf66c5c21c6073f612264fa2642
Gawa <precpaw@op.pl>
2017-11-24 18:31:42 +0100

    Fixing error handling

--- src/main/java/pg/ui/exception/UIError.java
+++ src/main/java/pg/ui/exception/UIError.java
@@ -2,22 +2,26 @@ package pg.ui.exception;
 
 /**Created by Gawa 2017-10-07*/
 public enum UIError {
-    LOAD_BUNDLE,
-    LOAD_PROPERTIES,
-    SAVE_PROPERTIES,
-    NULL_ARGUMENT,
-    NO_ARGUMENTS,
-    PREPARATION,
-    NULL_ERROR_CODE,
-    BUTTON_IMG,
-    DESTINATION_NOT_CREATED,
-    LAUNCH_PROGRAM,
-    CANCELLED_TASK,
-    ARGUMENT_WRONG_VALUE,
-    GET_TORRENTS,
-    LOGIN_DS,
-    CREATE_TASK,
-    LIST_OF_TASK,
-    DELETE_TASK,
-    ;
+    LOAD_BUNDLE("Can not load bundle."),
+    SAVE_PROPERTIES("Can not save properties."),
+    BUTTON_IMG("Can not load image for button."),
+    LAUNCH_PROGRAM("Problem with starting program."),
+    GET_TORRENTS("Problem with getting torrents."),
+    LOGIN_DS("Problem with log in to disk station."),
+    CREATE_TASK("Problem with creating task."),
+    LIST_OF_TASK("Problem with getting list of task from disk station."),
+    DELETE_TASK("Problem with deleting task on disk station"),
+    IMDB("Problem with downloading by imdb."),
+    SHORTCUT("Problem wen shortcut was used."),
+    FAVOURITES("Problem with downloading favourites.");
+
+    private String msg;
+
+    UIError(String msg) {
+        this.msg = msg;
+    }
+
+    public String msg() {
+        return msg;
+    }
 }
