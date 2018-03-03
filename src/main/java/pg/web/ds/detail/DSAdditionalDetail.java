98baf63e0c7dc8aeb4af0808d04faa9c583cc4cb
Gawa <precpaw@op.pl>
2018-03-03 22:18:21 +0100

    Updating DS model.

--- src/main/java/pg/web/ds/detail/DSAdditionalDetail.java
+++ src/main/java/pg/web/ds/detail/DSAdditionalDetail.java
@@ -5,71 +5,85 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 /** Created by Gawa 2018-03-03 */
 public class DSAdditionalDetail {
 
+    @JsonProperty("completed_time")
+    private long completedTime;
     @JsonProperty("connected_leechers")
     private long connectedLeechers;
+    @JsonProperty("connected_peers")
+    private long connectedPeers;
     @JsonProperty("connected_seeders")
     private long connectedSeeders;
     @JsonProperty("create_time")
     private String createTime;
     private String destination;
     private String priority;
+    @JsonProperty("seedelapsed")
+    private long seedElapsed;
+    @JsonProperty("started_time")
+    private long startedTime;
     @JsonProperty("total_peers")
     private long totalPeers;
+    @JsonProperty("total_pieces")
+    private long totalPieces;
+    @JsonProperty("unzip_password")
+    private String unzipPassword;
     private String uri;
+    @JsonProperty("waiting_seconds")
+    private long waitingSeconds;
+
+    public long getCompletedTime() {
+        return completedTime;
+    }
 
     public long getConnectedLeechers() {
         return connectedLeechers;
     }
 
-    public void setConnectedLeechers(long connectedLeechers) {
-        this.connectedLeechers = connectedLeechers;
+    public long getConnectedPeers() {
+        return connectedPeers;
     }
 
     public long getConnectedSeeders() {
         return connectedSeeders;
     }
 
-    public void setConnectedSeeders(long connectedSeeders) {
-        this.connectedSeeders = connectedSeeders;
-    }
-
     public String getCreateTime() {
         return createTime;
-    }
-
-    public void setCreateTime(String createTime) {
-        this.createTime = createTime;
     }
 
     public String getDestination() {
         return destination;
     }
 
-    public void setDestination(String destination) {
-        this.destination = destination;
-    }
-
     public String getPriority() {
         return priority;
     }
 
-    public void setPriority(String priority) {
-        this.priority = priority;
+    public long getSeedElapsed() {
+        return seedElapsed;
+    }
+
+    public long getStartedTime() {
+        return startedTime;
     }
 
     public long getTotalPeers() {
         return totalPeers;
     }
 
-    public void setTotalPeers(long totalPeers) {
-        this.totalPeers = totalPeers;
+    public long getTotalPieces() {
+        return totalPieces;
+    }
+
+    public String getUnzipPassword() {
+        return unzipPassword;
     }
 
     public String getUri() {
         return uri;
     }
 
-    public void setUri(String uri) {
-        this.uri = uri;
+    public long getWaitingSeconds() {
+        return waitingSeconds;
     }
 }
--- src/main/java/pg/web/ds/detail/DSFileDetail.java
+++ src/main/java/pg/web/ds/detail/DSFileDetail.java
@@ -6,40 +6,34 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class DSFileDetail {
 
     private String filename;
+    private long index;
     private String priority;
     private String size;
     @JsonProperty("size_downloaded")
     private String sizeDownloaded;
+    private boolean wanted;
 
     public String getFilename() {
         return filename;
     }
 
-    public void setFilename(String filename) {
-        this.filename = filename;
+    public long getIndex() {
+        return index;
     }
 
     public String getPriority() {
         return priority;
     }
 
-    public void setPriority(String priority) {
-        this.priority = priority;
-    }
-
     public String getSize() {
         return size;
-    }
-
-    public void setSize(String size) {
-        this.size = size;
     }
 
     public String getSizeDownloaded() {
         return sizeDownloaded;
     }
 
-    public void setSizeDownloaded(String sizeDownloaded) {
-        this.sizeDownloaded = sizeDownloaded;
+    public boolean isWanted() {
+        return wanted;
     }
 }
